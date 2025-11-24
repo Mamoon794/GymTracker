2c3b3bf0d031ffd966c9339b97a525f05ec3e88b
Mamoon Akhtar <mamoon.akhtar97@gmail.com>
2025-11-24 00:33:05 -0500

    Initial Commit

---
+++ Gym Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gym Tracker
+//
+//  Created by Mamoon Akhtar on 2025-11-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gym Tracker/Gym_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gym_TrackerApp.swift
+//  Gym Tracker
+//
+//  Created by Mamoon Akhtar on 2025-11-24.
+//
+
+import SwiftUI
+
+@main
+struct Gym_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
